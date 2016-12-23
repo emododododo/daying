d3d58943c7d0e6f97cb25f4a7735bd9258589e71
smackxu <smackxu@raventech.com>
2016-12-23 14:24:45 +0800

    Move dev handle to window.js form application.js

--- src/main/services/application.js
+++ src/main/services/application.js
@@ -1,8 +1,6 @@
-import { join } from 'path';
-import { create } from './window';
+import { create, getPath } from './window';
 
 export function init() {
   const win = create({ width: 800, height: 600 });
-  win.loadURL(`http://0.0.0.0:4010/index.html`);
-  // win.loadURL(`file://${join($dirname, '..', 'pages')}/main.html`);
+  win.loadURL(getPath());
 }
