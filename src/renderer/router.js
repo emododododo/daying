f1b962fcc6d8ef5f029c12a65eb96aef7e7670a3
smackxu <smackxu@raventech.com>
2017-01-06 12:01:29 +0800

    add reset.css

--- src/renderer/router.js
+++ src/renderer/router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route } from 'dva/router';
 import ListPage from './routes/itemList';
+import './utils/reset.css';
 
 function RouterConfig({ history }) {
   return (
