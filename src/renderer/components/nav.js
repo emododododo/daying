619549df16d6154e63c3685abe0fe870c87f9fb6
smackxu <smackxu@raventech.com>
2017-01-06 12:01:10 +0800

    update nav component

--- src/renderer/components/nav.js
+++ src/renderer/components/nav.js
@@ -1,28 +1,81 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import styles from './nav.css';
 
 const itemNav = [{
-  title:'知乎',
-  id: 'zhihu',
+  title: '知乎日报',
+  id: 'dailyZhihu',
 }, {
-  title:'1111',
-  id: '2222',
+  title: '简书',
+  id: 'jianshu',
 }, {
-  title:'3333',
-  id: '44444',
- }]
+  title: '前端',
+  id: 'qianduan',
+}, {
+  title: 'github',
+  id: 'github',
+}, {
+  title: 'cnblogs',
+  id: 'cnblogs',
+}, {
+  title: 'csdn',
+  id: 'csdn',
+}, {
+  title: 'wanqu',
+  id: 'wanqu',
+}, {
+  title: 'ithome',
+  id: 'ithome',
+}, {
+  title: 'solidot',
+  id: 'solidot',
+}];
 
-function Nav(itemList) {
-  return (
-    <div>
-      <ul>
-        {
-          itemNav.map((item, index) => {
-            return <li key={index}><a>{item.title}</a></li>;
-          })
-        }
-      </ul>
-    </div>
-  );
+class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSelected: 0,
+      isFolded: true,
+    };
+  }
+
+  onClickWrapper() {
+    const isFolded = this.state.isFolded;
+    this.setState({
+      isFolded: !isFolded,
+    });
+  }
+
+  onChangeTitle(index) {
+    this.setState({
+      isSelected: index,
+    });
+    this.props.onChangeTitle(itemNav[index].id);
+  }
+
+  render() {
+    const isSelected = this.state.isSelected;
+    const isFolded = this.state.isFolded;
+    const listClassName = isFolded ? styles.list : styles['list--active'];
+    const onClickWrapper = this.onClickWrapper.bind(this);
+    const onChangeTitle = this.onChangeTitle.bind(this);
+
+    return (
+      <div onClick={onClickWrapper}>
+        <p>{itemNav[isSelected].title}</p>
+        <ul className={listClassName}>
+          {
+            itemNav.map((item, index) => {
+              const onChange = () => {
+                onChangeTitle(index);
+              };
+              return <li key={index} onClick={onChange} ><a>{item.title}</a></li>;
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Nav;
