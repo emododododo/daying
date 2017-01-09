cc6172b115756e5bb2e38215671b1b912527cb4c
smackxu <smackxu@raventech.com>
2017-01-09 15:01:24 +0800

    don't use poxy

--- src/renderer/services/list.js
+++ src/renderer/services/list.js
@@ -1,6 +1,6 @@
 import request from '../utils/request';
-import qs from 'qs';
+// import qs from 'qs';
 
 export async function query(params) {
-  return request(`/api/resources?name=${params.queryName}`);
+  return request(`http://107.170.52.153:4007/api/resources?name=${params.queryName}`);
 }
