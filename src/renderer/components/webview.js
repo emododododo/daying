9e3cb9d39719e65f7b1bef5f3087c81afe6ccfd2
smackxu <smackxu@raventech.com>
2017-01-10 11:52:21 +0800

    add loading and crash on webview component

--- src/renderer/components/webview.js
+++ src/renderer/components/webview.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from './webview.css';
+import Loading from './loading';
 
 const openExternal = require('electron').remote.shell.openExternal;
 const arrowLeft = require('../assets/arrow_left.png');
 const reload = require('../assets/reload.png');
 const browser = require('../assets/browser.png');
+const error = require('../assets/error.png');
 
 class Webview extends React.Component {
 
@@ -13,6 +15,9 @@ class Webview extends React.Component {
     this.state = {
       canGoBack: false,
       canGoForward: false,
+      isLoading: false,
+      crashed: false,
+      errorCode: '',
     };
   }
 
@@ -25,9 +30,47 @@ class Webview extends React.Component {
     });
 
     webviewElement.addEventListener('did-navigate', () => {
+      const canGoBack = webviewElement.canGoBack();
+      const canGoForward = webviewElement.canGoForward();
+      if (this.state.canGoBack !== canGoBack) {
+        this.setState({
+          canGoBack,
+        });
+      }
+      if (this.state.canGoForward !== canGoForward) {
+        this.setState({
+          canGoForward,
+        });
+      }
+    });
+
+    // 浏览器崩溃或者加载失败
+    webviewElement.addEventListener('did-fail-load', (e) => {
+      if (this.props.url === e.validatedURL && e.errorCode !== -3) {
+        this.setState({
+          crashed: true,
+          errorCode: e.errorCode,
+        });
+      }
+    });
+
+    webviewElement.addEventListener('crashed', () => {
       this.setState({
-        canGoBack: webviewElement.canGoBack(),
-        canGoForward: webviewElement.canGoForward(),
+        crashed: true,
+      });
+    });
+
+    // Loading
+    webviewElement.addEventListener('did-start-loading', () => {
+      this.setState({
+        isLoading: true,
+        crashed: false,
+      });
+    });
+
+    webviewElement.addEventListener('did-stop-loading', () => {
+      this.setState({
+        isLoading: false,
       });
     });
 
@@ -44,8 +87,10 @@ class Webview extends React.Component {
     };
   }
 
-  componentWillReceiveProps() {
+  componentWillReceiveProps(nextProps) {
     const webviewElement = document.getElementById('foo');
+    webviewElement.clearHistory();
+    webviewElement.loadURL(nextProps.url);
     this.setState({
       canGoBack: webviewElement.canGoBack(),
     });
@@ -59,20 +104,17 @@ class Webview extends React.Component {
   }
 
   render() {
-    const itemList = this.props;
-    const url = itemList.url || (itemList.list[0] && itemList.list[0].url) || '#';
+    const url = '#';
+    const crashedClassName = this.state.crashed ? styles['crashed--active'] : styles.crashed;
     const goBackClassNames = this.state.canGoBack ? `${styles.goBack} ${styles.canGoBack}` : styles.goBack;
     const goForwardClassNames = this.state.canGoForward ? `${styles.goForward} ${styles.canGoForward}` : styles.goForward;
-    let reloadClassNames = styles.reload;
-    let browserClassNames = styles.browser;
+    const reloadClassNames = `${styles.reload} ${styles['icon-active']}`;
+    const browserClassNames = `${styles.browser} ${styles['icon-active']}`;
+
     const onBrowser = this.onBrowser.bind(this);
-    if (url && url !== '#') {
-      reloadClassNames = `${styles.reload} ${styles['icon-active']}`;
-      browserClassNames = `${styles.browser} ${styles['icon-active']}`;
-    }
 
     return (
-      <div className={styles.wrapper}>
+      <Loading className={styles.wrapper} isLoading={this.state.isLoading}>
         <div className={styles['webview-bar']}>
           <a onClick={this.onGoBack} className={styles['goBack-wrapper']}><img className={goBackClassNames} src={arrowLeft} alt="" /></a>
           <a onClick={this.onGoForward} className={styles['goForward-wrapper']}><img className={goForwardClassNames} src={arrowLeft} alt="" /></a>
@@ -80,7 +122,13 @@ class Webview extends React.Component {
           <a onClick={onBrowser} className={styles['browser-wrapper']}><img className={browserClassNames} src={browser} alt="" /></a>
         </div>
         <webview className={styles.webview} id="foo" src={url} />
-      </div>
+        <div className={crashedClassName}>
+          <div className={styles['crashed-content']}>
+            <img className={goBackClassNames} src={error} alt="" />
+            <span>出现错误 {this.state.errorCode}</span>
+          </div>
+        </div>
+      </Loading>
     );
   }
 }
