711bce5f836a7b8fd1d54b4d9e7a2a60dc0eb500
smackxu <smackxu@raventech.com>
2017-01-10 15:09:32 +0800

    change window title

--- src/main/services/application.js
+++ src/main/services/application.js
@@ -1,6 +1,10 @@
 import { create, getPath } from './window';
 
 export function init() {
-  const win = create({ width: 1024, height: 768 });
+  const win = create({
+    width: 1024,
+    height: 768,
+    title: '打烊',
+  });
   win.loadURL(getPath());
 }
