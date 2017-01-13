3b64aa439387955d4c92100f5c7eda12fe42b8f2
smackxu <smackxu@raventech.com>
2017-01-13 17:00:15 +0800

    can copy and paste

--- src/main/services/menu.js
+++ src/main/services/menu.js
@@ -19,6 +19,13 @@ function getTemplate() {
     {
       label: '配置',
       submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' },
         {
           label: '配置',
           click: () => {
