8f0e3d06cc156f89711212b9a26398de16df27b6
smackxu <smackxu@raventech.com>
2017-01-16 18:00:21 +0800

    finished subscribesEdit on edit page

---
+++ src/renderer/components/subscribesEdit.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { Checkbox, CheckboxGroup } from 'react-checkbox-group';
+import styles from './subscribesEdit.css';
+
+const dialog = require('electron').remote.dialog;
+
+class CheckBox extends React.Component {
+  constructor(props) {
+    super(props);
+    const slectedIdList = props.navList.map(item => item.id);
+    this.state = {
+      slectedIdList,
+    };
+  }
+
+  onConfirm() {
+    // 限制订阅数量
+    // if (this.state.slectedIdList.length <= 0 || this.state.slectedIdList.length > 10) {
+      // dialog.showErrorBox('❌', `您只能订阅 1~10 个信息源，请重新选择。但您选择了${this.state.slectedIdList.length}个信息源`);
+      // return;
+    // }
+
+    const objList = {};
+    this.state.slectedIdList.forEach((item) => {
+      objList[item] = item;
+    });
+
+    // 处理选中数据
+    const updateList = this.props.allNavList.reduce((arr, item) => {
+      let result = arr;
+      if (item.data && item.data.length > 0) {
+        const resultJ = item.data.reduce((arrI, itemI) => {
+          const resultI = arrI;
+          if (objList[itemI.id]) {
+            resultI.push({
+              id: itemI.id,
+              title: itemI.title,
+            });
+          }
+          return resultI;
+        }, []);
+        result = result.concat(resultJ);
+      } else if (objList[item.id]) {
+        result.push({
+          id: item.id,
+          title: item.title,
+        });
+      }
+      return result;
+    }, []);
+
+    // Dispatch updateNavList action
+    this.props.dispatch({
+      type: 'itemList/updateNavList',
+      payload: {
+        navList: updateList,
+      },
+    });
+    // console.log(dialog.showMessageBox);
+    // dialog.showMessageBox({
+    //   type: 'info',
+    //   title: 'fafs',
+    //   buttons: ['确定', '取消'],
+    //   message: 'fjsdjafldsjflksjdkl',
+    // }, (response) => {
+    //   console.log(response);
+    // });
+    dialog.showErrorBox('✔️', '订阅源更改成功');
+  }
+
+  navListChanged(newIdList) {
+    this.setState({
+      slectedIdList: newIdList,
+    });
+  }
+
+  render() {
+    const allNavList = this.props.allNavList;
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles['checkBox-wrapper']}>
+          <CheckboxGroup name="navList" value={this.state.slectedIdList} onChange={this.navListChanged.bind(this)}>
+            {
+              allNavList.map((item, index) => {
+                // itemListJSX
+                let itemListJSX = (
+                  <div className={styles['item-content']}>
+                    <Checkbox value={item.id} />
+                    <span>热门</span>
+                  </div>
+                );
+                if (item.data && item.data.length > 0) {
+                  itemListJSX = item.data.map((itemI, indexI) => {
+                    return (
+                      <div key={indexI} className={styles['item-content']}>
+                        <Checkbox value={itemI.id} />
+                        <span>{itemI.title}</span>
+                      </div>
+                    );
+                  });
+                }
+
+                return (
+                  <div className={styles.wrapper} key={index}>
+                    <div className={styles.title}>{item.title}</div>
+                    <div className={styles.itemList}>{itemListJSX}</div>
+                  </div>
+                );
+              })
+            }
+          </CheckboxGroup>
+        </div>
+        <button className={styles['confirm-button']} onClick={this.onConfirm.bind(this)}>确定</button>
+      </div>
+    );
+  }
+}
+
+export default CheckBox;
--- src/renderer/routes/EditPage.js
+++ src/renderer/routes/EditPage.js
@@ -1,9 +1,23 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'dva';
-import styles from './IndexPage.css';
-import CheckBox from '../components/checkBox';
+import styles from './EditPage.css';
+import SubscribesEdit from '../components/subscribesEdit';
+import About from '../components/about';
 
 class EditPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.editNav = ['订阅源', '关于'];
+    this.state = {
+      editNavSelected: 0,
+    };
+  }
+
+  selectedNav(index) {
+    this.setState({ editNavSelected: index });
+  }
+
   render() {
     const { itemList, dispatch } = this.props;
     const navList = itemList.navList;
@@ -12,9 +26,37 @@ class EditPage extends React.Component {
       allNavList: itemList.allNavList,
       dispatch,
     };
+
+    let editContent = '';
+    switch (this.state.editNavSelected) {
+      case 0:
+        editContent = <SubscribesEdit {...checkBoxProps} />;
+        break;
+      case 1:
+        editContent = <About />;
+        break;
+      default:
+        editContent = <SubscribesEdit {...checkBoxProps} />;
+    }
+
     return (
-      <div className={styles.normal}>
-        <CheckBox {...checkBoxProps} />
+      <div className={styles.editPage}>
+        <div className={styles.editNav}>
+          {
+            this.editNav.map((item, index) => {
+              let className = styles.editNavItem;
+              if (this.state.editNavSelected === index) {
+                className += ` ${styles['editNavItem--selected']}`;
+              }
+              return (
+                <a key={index} onClick={() => { this.selectedNav(index); }}>
+                  <p className={className}>{item}</p>
+                </a>
+              );
+            })
+          }
+        </div>
+        <div className={styles.editContent}>{editContent}</div>
       </div>
     );
   }
