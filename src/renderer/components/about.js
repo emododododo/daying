0729fc6b31ae4538ca2ce6e17fd18cff88e5ae70
smackxu <smackxu@raventech.com>
2017-01-16 18:01:41 +0800

    Add about component

---
+++ src/renderer/components/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './about.css';
+
+function About() {
+  return (
+    <div className={styles['about-wrapper']}>about</div>
+  );
+}
+
+export default About;
