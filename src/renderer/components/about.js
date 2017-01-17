86cfa3d886472e838ce4f911ee39527430ae33c0
smackxu <smackxu@raventech.com>
2017-01-17 17:47:53 +0800

    add about page

--- src/renderer/components/about.js
+++ src/renderer/components/about.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import styles from './about.css';
 
+const openExternal = require('electron').remote.shell.openExternal;
+
 function About() {
+  const about = {
+    version: '0.1.0',
+    author: 'smacgg&emodo',
+    mail: 'xysmackdown@gmail.com',
+    github: <a href="#1" onClick={() => { openExternal('https://github.com/emododododo/daying'); }}>https://github.com/emododododo/daying</a>,
+  };
   return (
-    <div className={styles['about-wrapper']}>about</div>
+    <div className={styles['about-wrapper']}>
+      {
+        Object.keys(about).map((item, index) => {
+          return (
+            <div key={index} className={styles.row}>
+              <p>{item}:</p>
+              <p>{about[item]}</p>
+            </div>
+          );
+        })
+      }
+    </div>
   );
 }
 
